3be81528922a6d2b5e9c4d95e9105abb2bb968ef
Sharavanan Sivaraya <46288550+Sharee-04@users.noreply.github.com>
2023-02-25 00:12:48 +0000

    Update Go_to_Sleep.js

--- Go_to_Sleep.js
+++ Go_to_Sleep.js
@@ -1,10 +1,20 @@
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
 
-var dt = new Date();
-var H = dt.getHours();
-var M = dt.getMinutes();
-
-document.getElementById("time").innerHTML = "It is " + H + ":" + M + ".";
-
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
 
 
 
