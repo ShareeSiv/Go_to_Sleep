a5f17140a1d35c91c436f140a04ca2480a45fa82
Sharavanan Sivaraya <46288550+Sharee-04@users.noreply.github.com>
2023-02-25 00:16:30 +0000

    Update Go_to_Sleep.js

--- Go_to_Sleep.js
+++ Go_to_Sleep.js
@@ -1,20 +1,24 @@
-(function () {
-    function checkTime(i) {
-        return (i < 10) ? "0" + i : i;
-    }
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 
-    function startTime() {
-        var today = new Date(),
-            h = checkTime(today.getHours()),
-            m = checkTime(today.getMinutes()),
-            s = checkTime(today.getSeconds());
-        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-        t = setTimeout(function () {
-            startTime()
-        }, 500);
-    }
-    startTime();
-})();
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout(function() {
+    startTime()
+  }, 500);
+}
+startTime();
 
 
 
